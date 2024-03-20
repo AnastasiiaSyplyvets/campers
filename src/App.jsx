30e48bdc84f1ee2c58414f48afc4bf2a9d2da675
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2024-03-20 20:46:47 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import ErrorPage from 'pages/ErrorPage/ErrorPage';
 const test = import.meta.env.VITE_API_TEST;
 
 function App() {
-  console.log(test);
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
