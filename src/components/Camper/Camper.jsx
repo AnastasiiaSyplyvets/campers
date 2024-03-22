9ba643b24826964f290f64319612ddcd71a6239a
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2024-03-22 20:33:45 +0100

    3

--- src/components/Camper/Camper.jsx
+++ src/components/Camper/Camper.jsx
@@ -1,16 +1,45 @@
 import { useSelector } from 'react-redux';
+import styles from './Camper.module.css';
+import sprite from '../../assets/sprite.svg';
 
 export const Camper = () => {
-  const camper = useSelector((state) => state.campers);
+  const campers = useSelector((state) => state.campers);
+  console.log(campers);
 
-  return (
-    <div>
-      <img width="50px" src={camper.avatar}></img>
-      <div>
-        <h2>{camper.name}</h2>
-        <div></div>
-        <p></p>
+  const ratingCount = (reviews) => {
+    let total = 0;
+    for (let i = 0; i < reviews.length; i += 1) {
+      total += reviews[i].reviewer_rating;
+    }
+    return total / reviews.length;
+  };
+
+  return campers.campers.map((camper, index) => {
+    return (
+      <div key={index}>
+        <img width="219px" src={camper.gallery[0]}></img>
+
+        <div>
+          <div>
+            <h2>{camper.name}</h2>
+            <p>&euro; {camper.price}</p>
+          </div>
+          <div>
+            <svg width="16px" height="16px">
+              <use xlinkHref={sprite + '#icon-star'}></use>
+            </svg>
+            <p>
+              {ratingCount(camper.reviews)} ( {camper.reviews.length} Rewiews)
+            </p>
+            <svg width="12px" height="14px" fill="#101828" stroke="#ffffff">
+              <use xlinkHref={sprite + '#icon-map-pin-1'}></use>
+            </svg>
+            <p>{camper.location}</p>
+          </div>
+          {/* <div></div>
+          <p></p> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  });
 };
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,11 +1,108 @@
 import { useDispatch } from 'react-redux';
 
 import { fetchCamperList } from '../../redux/operations';
+import { Camper } from '../../components/Camper/Camper';
+
+import sprite from '../../assets/sprite.svg';
+import styles from '../CatalogPage/CatalogPage.module.css';
 const CatalogPage = () => {
   const dispatch = useDispatch();
 
   dispatch(fetchCamperList());
-  return <div>catalog</div>;
+  return (
+    <section>
+      <div>
+        <div className={styles.locationContainer}>
+          <label htmlFor="location">Location</label>
+          <svg
+            className={styles.locationSvg}
+            fill="#101828"
+            stroke="#ffffff"
+            width="18px"
+            height="20px"
+          >
+            <use xlinkHref={sprite + '#icon-map-pin-1'}></use>
+          </svg>
+          <input
+            className={styles.locationInput}
+            id="location"
+            placeholder="Kyiv, Ucraine"
+          ></input>
+        </div>
+
+        <div className={styles.container}>
+          <h3 className={styles.title}>Filters</h3>
+          <ul className={styles.list}>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use xlinkHref={sprite + '#icon-Vertical-container'}></use>
+                </svg>
+              </button>
+            </li>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use xlinkHref={sprite + '#icon-Horizontal-container'}></use>
+                </svg>
+              </button>
+            </li>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use
+                    xlinkHref={sprite + '#icon-Horizontal-container-1'}
+                  ></use>
+                </svg>
+              </button>
+            </li>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use xlinkHref={sprite + '#icon-Container'}></use>
+                </svg>
+              </button>
+            </li>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use xlinkHref={sprite + '#icon-Container-1'}></use>
+                </svg>
+              </button>
+            </li>
+          </ul>
+          <h3 className={styles.title}>Vehicle type</h3>
+          <ul className={styles.list}>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use xlinkHref={sprite + '#icon-Button-container'}></use>
+                </svg>
+              </button>
+            </li>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use xlinkHref={sprite + '#icon-Button-container-1'}></use>
+                </svg>
+              </button>
+            </li>
+            <li>
+              <button className={styles.camperBtn}>
+                <svg width="95px" height="95px">
+                  <use xlinkHref={sprite + '#icon-Button-container-2'}></use>
+                </svg>
+              </button>
+            </li>
+          </ul>
+          <button className={styles.searchBtn}>Search</button>
+        </div>
+      </div>
+      <div>
+        <Camper />
+      </div>
+    </section>
+  );
 };
 
 export default CatalogPage;
