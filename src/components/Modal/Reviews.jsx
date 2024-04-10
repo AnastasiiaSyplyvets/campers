d68f46fa448084091e95edc0e157ad9f73a49bb8
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2024-04-10 19:18:32 +0200

    form re-written to formik

--- src/components/Modal/Reviews.jsx
+++ src/components/Modal/Reviews.jsx
@@ -1,6 +1,9 @@
 import styles from '../Modal/Features.module.css';
 import css from '../Modal/Reviews.module.css';
 import sprite from '../../assets/sprite.svg';
+
+import { Formik } from 'formik';
+
 export const Reviews = ({ camper }) => {
   const stars = (review) => {
     let starsCount = [];
@@ -43,7 +46,7 @@ export const Reviews = ({ camper }) => {
         <p className={styles.formText}>
           Stay connected! We are always ready to help you.
         </p>
-        <form className={styles.form}>
+        {/* <form className={styles.form}>
           <input className={styles.inputForm} type="text" placeholder="Name" />
           <input className={styles.inputForm} type="text" placeholder="Email" />
           <input
@@ -59,7 +62,91 @@ export const Reviews = ({ camper }) => {
           <button className={styles.formBtn} type="submit">
             Send
           </button>
-        </form>
+        </form> */}
+        <Formik
+          initialValues={{ name: '', email: '', bookingDate: '', comment: '' }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Email is required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            if (!values.name) {
+              errors.name = 'Name is required';
+            }
+            if (!values.bookingDate) {
+              errors.bookingDate = 'Date is required';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log('submitted');
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form className={styles.form} onSubmit={handleSubmit}>
+              <input
+                className={styles.inputForm}
+                type="text"
+                placeholder="Name"
+                name="name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+              />
+              {errors.name && touched.name && errors.name}
+              <input
+                className={styles.inputForm}
+                placeholder="Email"
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {errors.email && touched.email && errors.email}
+              <input
+                className={styles.inputForm}
+                type="text"
+                placeholder="Booking date"
+                name="bookingDate"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.bookingDate}
+              />
+              {errors.bookingDate && touched.bookingDate && errors.bookingDate}
+              <input
+                className={styles.commentInput}
+                type="text"
+                placeholder="Comment"
+                name="comment"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.comment}
+              />
+              {errors.comment && touched.comment && errors.comment}
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={styles.formBtn}
+              >
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
